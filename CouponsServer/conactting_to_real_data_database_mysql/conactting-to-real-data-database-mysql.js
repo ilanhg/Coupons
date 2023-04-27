925b23a0bf16743913975b82d70960690335cfeb
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-04-27 19:37:17 +0300

    adding more info

--- CouponsServer/conactting_to_real_data_database_mysql/conactting-to-real-data-database-mysql.js
+++ CouponsServer/conactting_to_real_data_database_mysql/conactting-to-real-data-database-mysql.js
@@ -1,27 +1,28 @@
 const mysql = require("mysql");
 const connectToRealMysql = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
   // database: "UsersCouponMysqlDatabase",
-   database: "UsersDatabase",
+  database: process.env.DATABASE,
 });
 connectToRealMysql.connect((err) => {
   if (err) {
     console.log(err);
-  }});
-  function savingCouponsIntoRealMysqlOrGettingCouponsFromRealMysql(sql) {
-    return new Promise((resolve, reject) => {
-      connectToRealMysql.query(sql, (err, result) => {
-        if (err) {
-          reject(err);
-          return;
-        } else {
-          resolve(result);
-        }
-      });
+  }
+});
+function savingCouponsIntoRealMysqlOrGettingCouponsFromRealMysql(sql) {
+  return new Promise((resolve, reject) => {
+    connectToRealMysql.query(sql, (err, result) => {
+      if (err) {
+        reject(err);
+        return;
+      } else {
+        resolve(result);
+      }
     });
-   }
-  module.exports = {
-    savingCouponsIntoRealMysqlOrGettingCouponsFromRealMysql,
-  };
+  });
+}
+module.exports = {
+  savingCouponsIntoRealMysqlOrGettingCouponsFromRealMysql,
+};
