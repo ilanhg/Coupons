681f9885b3d1c98caecb7697d573bf3c5797e1c2
Katya Rukosuev <72752681+RuKatya@users.noreply.github.com>
2023-04-27 19:27:48 +0300

    adding env and gitignore

--- CouponsServer/main.js
+++ CouponsServer/main.js
@@ -5,28 +5,30 @@ server.use(express.json());
 const userControler = require("./controlers/user-controler");
 const loginUsersController = require("./controlers/user-login-controler")
 // const couponsController = require('./controlers/coupons-controler')
-const PORT = 4000;
-const SERVER_DISPLAY= 3000;
+const PORT = process.env.PORT || 4000;
+const SERVER_DISPLAY = 3000;
 // const connectToRealMysql = require('./conactting_to_real_data_database_mysql/conactting-to-real-data-database-mysql')
 // const cors = server.use(cors({
 //    origin: ['http://localhost:3000/','ziv','bar','emanuel'...]
 // }));
 server.use(cors());
 
+
+
 // "http://localhost:4000/api/register/users/"
-server.use("/api/register/users",userControler);
+server.use("/api/register/users", userControler);
 // //http://localhost:4000/api/login
-server.use("/api/register/login",loginUsersController);
- //http://localhost:4000/api/coupons
+server.use("/api/register/login", loginUsersController);
+//http://localhost:4000/api/coupons
 // server.use("/api/coupons",couponsController);
-server.use("*",(request,response)=>{
+server.use("*", (request, response) => {
   response.json("not found!!")
 })
 
 server.listen(PORT, () => {
-  setInterval(() => {
-      console.log("main.js is up and running");
-  }, SERVER_DISPLAY);
+  // setInterval(() => {
+  console.log("main.js is up and running");
+  // }, SERVER_DISPLAY);
 });
 
 
